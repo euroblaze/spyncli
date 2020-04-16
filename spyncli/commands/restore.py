b0f5f921bcf6506480b337b1ebbbcea999ea8297
Kristijan <m44@euroblaze.de>
2020-04-16 08:17:07 +0200

    indent fix on RESTORE

--- spyncli/commands/restore.py
+++ spyncli/commands/restore.py
@@ -17,10 +17,10 @@ class Restore(Base):
         auth_password = env.str("auth_password")
         dir_path = abspath(dirname(__file__))
 
-	if self.options['perform']:
-		response = requests.post(API + "backups/restore",
-					auth=(auth_username, auth_password),
-					data={'namespace': self.options['<namespace>'],
-						'name': self.options['<name>']})
-		print(response.status_code, response.content.decode())
+        if self.options['perform']:
+                response = requests.post(API + "backups/restore",
+                                        auth=(auth_username, auth_password),
+                                        data={'namespace': self.options['<namespace>'],
+                                              'name': self.options['<name>']})
+                print(response.status_code, response.content.decode())
 
